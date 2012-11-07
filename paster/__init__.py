3dd4e8666cc90322772f07ce674834f9abd5f73e
Rohan Jain <crodjer@gmail.com>
2012-11-07 13:48:04 +0530

    Bump version number
    Signed-off-by: Rohan Jain <crodjer@gmail.com>

--- paster/__init__.py
+++ paster/__init__.py
@@ -13,6 +13,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-version = '0.7'
+version = '0.8'
 import logging
 logging.basicConfig(format="%(asctime)-15s %(levelname)s %(message)s")
